701794984a48e8bd59e48f121320a05c35b03717
markmurdock11 <markmurdock11@gmail.com>
2020-10-31 19:24:34 -0500

    sql table almost working

--- Data/QuickDBD-export.sql
+++ Data/QuickDBD-export.sql
@@ -1,65 +1,72 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
+--Card Holder
 
-CREATE TABLE "CardHolder" (
-    "id" SERIAL   NOT NULL,
-    "Name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_CardHolder" PRIMARY KEY (
-        "id"
-     )
-);
-
-CREATE TABLE "CreditCard" (
-    "card_number" INT   NOT NULL,
-    "id_card_holder" INT   NOT NULL,
-    CONSTRAINT "pk_CreditCard" PRIMARY KEY (
-        "id_card_holder"
-     )
-);
-
-CREATE TABLE "Merchant" (
+CREATE TABLE "card_holder" (
     "id" SERIAL   NOT NULL,
     "name" VARCHAR(50)   NOT NULL,
-    "id_merchant_cat" INT   NOT NULL,
-    CONSTRAINT "pk_Merchant" PRIMARY KEY (
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
         "id"
      )
 );
 
-CREATE TABLE "MerchantCat" (
+-- Credit Card
+
+CREATE TABLE "credit_card" (
+    "card" VARCHAR(20)   NOT NULL,
+    "id_card_holder" INT   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+-- Merchant
+
+CREATE TABLE "merchant" (
     "id" SERIAL   NOT NULL,
-    "name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_MerchantCat" PRIMARY KEY (
+    "name" VARCHAR(255)   NOT NULL,
+    "id_merchant_category" INT   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
         "id"
      )
 );
 
-CREATE TABLE "Transaction" (
+-- Merchant Category
+
+CREATE TABLE "merchant_category" (
+    "id" SERIAL   NOT NULL,
+    "name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+-- Transaction
+
+CREATE TABLE "transaction" (
     "id" INT   NOT NULL,
     "date" TIMESTAMP   NOT NULL,
     "amount" FLOAT   NOT NULL,
-    "card" INT   NOT NULL,
+    "card" VARCHAR(20)   NOT NULL,
     "id_merchant" INT   NOT NULL,
-    CONSTRAINT "pk_Transaction" PRIMARY KEY (
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
         "id"
      )
 );
 
-ALTER TABLE "CardHolder" ADD CONSTRAINT "fk_CardHolder_id" FOREIGN KEY("id")
-REFERENCES "CreditCard" ("id_card_holder");
 
-ALTER TABLE "Merchant" ADD CONSTRAINT "fk_Merchant_id" FOREIGN KEY("id")
-REFERENCES "Transaction" ("id_merchant");
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_id_card_holder" FOREIGN KEY("id_card_holder")
+REFERENCES "card_holder" ("id");
 
-ALTER TABLE "Merchant" ADD CONSTRAINT "fk_Merchant_id_merchant_cat" FOREIGN KEY("id_merchant_cat")
-REFERENCES "MerchantCat" ("id");
+ALTER TABLE "credit_card" ADD CONSTRAINT "check_credit_card_length"  CHECK (char_length("card") <= 20);
 
-ALTER TABLE "Transaction" ADD CONSTRAINT "fk_Transaction_card" FOREIGN KEY("card")
-REFERENCES "CreditCard" ("card_number");
+--ALTER TABLE "credit_card" DROP CONSTRAINT check_credit_card_length
 
-ALTER TABLE "Transaction" ADD CONSTRAINT "fk_Transaction_id_merchant" FOREIGN KEY ("id_merchant")
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merchant_category" FOREIGN KEY("id_merchant_category")
+REFERENCES "merchant_category" ("id");
 
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
