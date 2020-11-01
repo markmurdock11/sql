fe619606713cd72eaef37fe37fa44d0f8976fcfc
markmurdock11 <markmurdock11@gmail.com>
2020-10-31 23:32:42 -0500

    finally was able to pull from sql into pandas

--- Data/QuickDBD-exportTHISONEWORKS.sql
+++ Data/QuickDBD-exportTHISONEWORKS.sql
@@ -2,59 +2,59 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "CardHolder" (
+CREATE TABLE "card_holder" (
     "id" SERIAL   NOT NULL,
     "name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_CardHolder" PRIMARY KEY (
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
         "id"
      )
 );
 
-CREATE TABLE "CreditCard" (
+CREATE TABLE "credit_card" (
     "card" VARCHAR   NOT NULL,
     "idcardholder" INT   NOT NULL,
-    CONSTRAINT "pk_CreditCard" PRIMARY KEY (
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
         "card"
      )
 );
 
-CREATE TABLE "Merchant" (
+CREATE TABLE "merchant" (
     "id" SERIAL   NOT NULL,
     "name" VARCHAR   NOT NULL,
     "id_merch_cat" INT   NOT NULL,
-    CONSTRAINT "pk_Merchant" PRIMARY KEY (
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
         "id"
      )
 );
 
-CREATE TABLE "MerchantCat" (
+CREATE TABLE "merchant_cat" (
     "id" SERIAL   NOT NULL,
     "name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_MerchantCat" PRIMARY KEY (
+    CONSTRAINT "pk_merchant_cat" PRIMARY KEY (
         "id"
      )
 );
 
-CREATE TABLE "Transaction" (
+CREATE TABLE "transaction" (
     "id" INT   NOT NULL,
     "date" TIMESTAMP   NOT NULL,
     "amount" FLOAT   NOT NULL,
     "card" VARCHAR   NOT NULL,
     "id_merchant" INT   NOT NULL,
-    CONSTRAINT "pk_Transaction" PRIMARY KEY (
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
         "id"
      )
 );
 
-ALTER TABLE "CreditCard" ADD CONSTRAINT "fk_CreditCard_idcardholder" FOREIGN KEY("idcardholder")
-REFERENCES "CardHolder" ("id");
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_idcardholder" FOREIGN KEY("idcardholder")
+REFERENCES "card_holder" ("id");
 
-ALTER TABLE "Merchant" ADD CONSTRAINT "fk_Merchant_id_merch_cat" FOREIGN KEY("id_merch_cat")
-REFERENCES "MerchantCat" ("id");
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merch_cat" FOREIGN KEY("id_merch_cat")
+REFERENCES "merchant_cat" ("id");
 
-ALTER TABLE "Transaction" ADD CONSTRAINT "fk_Transaction_card" FOREIGN KEY("card")
-REFERENCES "CreditCard" ("card");
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
 
-ALTER TABLE "Transaction" ADD CONSTRAINT "fk_Transaction_id_merchant" FOREIGN KEY("id_merchant")
-REFERENCES "Merchant" ("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
 
